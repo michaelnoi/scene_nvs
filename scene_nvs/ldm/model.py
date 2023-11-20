ba8b387c8760eaa31e70822ddd40e4f3912295b0
Michael Neumayr <michael.jun.neumayr@gmail.com>
2023-11-20 09:43:26 +0000

    Intermediate setup with CLIP vision model only working

--- scene_nvs/ldm/model.py
+++ scene_nvs/ldm/model.py
@@ -3,40 +3,62 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision
-from diffusers import AutoencoderKL, DDIMScheduler, UNet2DConditionModel
+import wandb
+from diffusers import (
+    AutoencoderKL,
+    DDIMScheduler,
+    EulerAncestralDiscreteScheduler,
+    UNet2DConditionModel,
+)
 from PIL import Image
 from tqdm import tqdm
-from transformers import CLIPTextModel, CLIPTokenizer
-
-import wandb
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPVisionModelWithProjection
 
 
 class SceneNVSNet(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.vae = AutoencoderKL.from_pretrained(
-            "stabilityai/stable-diffusion-2-base", subfolder="vae"
+            "stabilityai/stable-diffusion-2",
+            subfolder="vae",
+            variant="fp16",
         )
         self.unet = UNet2DConditionModel.from_pretrained(
-            "stabilityai/stable-diffusion-2-base", subfolder="unet"
+            "stabilityai/stable-diffusion-2",
+            subfolder="unet",
+            variant="fp16",
         )
-        self.tokenizer = CLIPTokenizer.from_pretrained(
-            "stabilityai/stable-diffusion-2-base", subfolder="tokenizer"
+        # self.tokenizer = CLIPTokenizer.from_pretrained(
+        #     "stabilityai/stable-diffusion-2", subfolder="tokenizer"
+        # )
+        # self.text_encoder = CLIPTextModel.from_pretrained(
+        #     "stabilityai/stable-diffusion-2", subfolder="text_encoder", variant="fp16",
+        # )
+        self.vision_encoder = CLIPVisionModelWithProjection.from_pretrained(
+            "weights/zero123plusplus",
+            subfolder="vision_encoder",
         )
-        self.text_encoder = CLIPTextModel.from_pretrained(
-            "stabilityai/stable-diffusion-2-base", subfolder="text_encoder"
-        )
+        # TODO: monitor: switched to custom setup of DDIM with v_prediction
+        # and linear schedule as advised in zero123pp paper
         self.noise_scheduler = DDIMScheduler.from_pretrained(
-            "stabilityai/stable-diffusion-2-base", subfolder="scheduler"
+            "configs/stable-diffusion-2-custom", subfolder="scheduler"
         )
 
         self.vae.requires_grad_(False)
-        self.text_encoder.requires_grad_(False)
+        # self.text_encoder.requires_grad_(False)
+        self.vision_encoder.requires_grad_(False)
         self.unet.requires_grad_(True)
 
     def forward(self, image_cond, image_target, T):
         x = self.vae.encode(image_target).latent_dist.sample()
         x = self.vae.config.scaling_factor * x
+
+        # TODO: go deep into diffusers' transformers and see if this makes sense
+        with torch.no_grad():
+            global_embeds = self.vision_encoder(image_cond).image_embeds.unsqueeze(-2)
+        # global_embeds.image_embeds.shape = torch.Size([1, 1024])                                                                                                                                        │
+        # global_embeds.image_embeds[0].shape = torch.Size([1024])
+        # global_embeds.last_hidden_state.shape = torch.Size([1, 257, 1280])
 
         noise = torch.randn_like(x).to(self.device)
         timesteps = torch.randint(
@@ -48,34 +70,37 @@ class SceneNVSNet(pl.LightningModule):
         timesteps = timesteps.long()
         noisy_x = self.noise_scheduler.add_noise(x, noise, timesteps)
 
-        uncond_input = self.tokenizer(
-            [""] * 1, padding="max_length", return_tensors="pt"
-        )
-        with torch.no_grad():
-            uncond_embeddings = self.text_encoder(
-                uncond_input.input_ids.to(self.device)
-            )[0]
+        # uncond_input = self.tokenizer(
+        #     [""] * 1, padding="max_length", return_tensors="pt"
+        # )
+        # with torch.no_grad():
+        #     uncond_embeddings = self.text_encoder(
+        #         uncond_input.input_ids.to(self.device)
+        #     )[0]
 
         condition = T
+
+        # clip_embeddings = self.noise_scheduler.scale_clip_embeddings(
+        #     global_embeds, timesteps
+        # )
 
         # Get the model prediction with conditioning
         pred = self.unet(
             sample=noisy_x,
             timestep=timesteps,
-            encoder_hidden_states=uncond_embeddings,
+            encoder_hidden_states=global_embeds,
             class_labels=condition,
         ).sample
 
         return pred, noise, timesteps, x
 
     def training_step(self, batch, batch_idx):
+        transform = torchvision.transforms.Resize((224, 224), antialias=False)
+
         image_cond = batch["image_cond"]
         image_target = batch["image_target"]
+        image_cond = transform(image_cond)
         T = batch["T"]
-
-        # permute (B, H, W, C) to (B, C, H, W)
-        image_cond = image_cond.permute(0, 3, 1, 2)
-        image_target = image_target.permute(0, 3, 1, 2)
 
         pred, noise, timesteps, x = self.forward(image_cond, image_target, T)
 
@@ -90,22 +115,29 @@ class SceneNVSNet(pl.LightningModule):
 
         loss = F.mse_loss(pred.float(), target.float(), reduction="mean")
 
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, sync_dist=True)
 
-        uncond_input = self.tokenizer(
-            [""] * 1, padding="max_length", return_tensors="pt"
-        )
-        with torch.no_grad():
-            uncond_embeddings = self.text_encoder(
-                uncond_input.input_ids.to(self.device)
-            )[0]
-        condition = T
+        # TODO: make dynamically adjustable
         if self.global_step % 5 == 0:
+            with torch.no_grad():
+                global_embeds = self.vision_encoder(image_cond).image_embeds.unsqueeze(
+                    -2
+                )
+
+            # uncond_input = self.tokenizer(
+            #     [""] * 1, padding="max_length", return_tensors="pt"
+            # )
+            # with torch.no_grad():
+            #     uncond_embeddings = self.text_encoder(
+            #         uncond_input.input_ids.to(self.device)
+            #     )[0]
+            condition = T
+
             num_inference_steps = 20  # Number of denoising steps
 
             generator = torch.manual_seed(0)
             latents = torch.randn(
-                (1, self.unet.in_channels, 256 // 8, 256 // 8),
+                (1, self.unet.config.in_channels, 256 // 8, 256 // 8),
                 dtype=torch.float16,
                 generator=generator,
             )
@@ -118,7 +150,7 @@ class SceneNVSNet(pl.LightningModule):
                     noise_pred = self.unet(
                         latent_model_input,
                         t,
-                        encoder_hidden_states=uncond_embeddings,
+                        encoder_hidden_states=global_embeds,
                         class_labels=condition,
                     ).sample
 
@@ -128,16 +160,33 @@ class SceneNVSNet(pl.LightningModule):
             with torch.no_grad():
                 image = self.vae.decode(latents).sample
 
+            # Log images
+            logger = self.logger.experiment
+            train_or_val = "train" if self.training else "val"
+
+            grid_target = torchvision.utils.make_grid(image_target, nrow=2)
+            im_target = grid_target.permute(1, 2, 0).clip(-1, 1) * 0.5 + 0.5
+            im_target = im_target.cpu()
+            im_target = Image.fromarray(np.array(im_target * 255).astype(np.uint8))
+            logger.log({f"Target Image {train_or_val}": wandb.Image(im_target)})
+
+            grid_cond = torchvision.utils.make_grid(image_cond, nrow=2)
+            im_cond = grid_cond.permute(1, 2, 0).clip(-1, 1) * 0.5 + 0.5
+            im_cond = im_cond.cpu()
+            im_cond = Image.fromarray(np.array(im_cond * 255).astype(np.uint8))
+            logger.log({f"Conditioning Image {train_or_val}": wandb.Image(im_cond)})
+
             grid = torchvision.utils.make_grid(image, nrow=2)
             im = grid.permute(1, 2, 0).clip(-1, 1) * 0.5 + 0.5
             im = im.cpu()
             im = Image.fromarray(np.array(im * 255).astype(np.uint8))
-            logger = self.logger.experiment
-            logger.log({"Sample generations": wandb.Image(im)})
+            logger.log({f"Sample generations {train_or_val}": wandb.Image(im)})
 
         return loss
 
+    # TODO: change back again, only for overfitting debug here
     def validation_step(self, batch, batch_idx):
+        return 0.0
         loss = self.training_step(batch, batch_idx)
         return loss
 
--- scene_nvs/train.py
+++ scene_nvs/train.py
@@ -1,28 +1,41 @@
+import os
+
 import lightning as pl
+import torch
 from data.datamodule import Scene_NVSDataModule
 from ldm.model import SceneNVSNet
 from pytorch_lightning.loggers import WandbLogger
 
-logger = WandbLogger(project="zero-shot-nvs")
+# for GPU
+torch.set_float32_matmul_precision("medium")
+
+# TODO: change all to proper shared W&B setup, maybe with config files
+project_name = "temp_scene_nvs"
+run_name = "debug_image_conditioning"
+logger = WandbLogger(project=project_name, name=run_name)
 
 datamodule = Scene_NVSDataModule(
     root_dir="/home/scannet/data/41b00feddb/iphone/",
     batch_size=1,
-    num_workers=0,
+    num_workers=4,
     image_size=256,
-    truncate_data=10,
+    truncate_data=1,
 )
-
 
 model = SceneNVSNet()
 
 trainer = pl.Trainer(
     devices=[0, 1],
     accelerator="gpu",
-    max_epochs=1,
-    precision=16,
+    max_epochs=10,
+    precision="16-mixed",
     strategy="deepspeed_stage_2",
+    # strategy="fsdp",
     enable_progress_bar=True,
     logger=logger,
+    log_every_n_steps=1,
 )
 trainer.fit(model, datamodule=datamodule)
+
+# Clean DeepSpeed checkpoints for debugging (they take up a lot of space)
+os.system(f"rm -rf {project_name}/")
